236e5789a835ce6d2cad315cd744014b2c3a0c47
Sunny Agarwal <sunny.agarwal@harness.io>
2026-02-12 14:26:38 +0530

    remove nested stack add ec2

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import aws_cdk as cdk
 
 from stacks.s3_stack import S3Stack
 from stacks.sqs_stack import SQSStack
-from stacks.app_foundation_stack import AppFoundationStack
+from stacks.ec2_stack import EC2Stack
 
 app = cdk.App()
 S3Stack(app, "S3Stack")
 SQSStack(app, "SQSStack")
-AppFoundationStack(app, "AppFoundationStack")
+EC2Stack(app, "EC2Stack")
 
 app.synth()
