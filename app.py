3d9937252d184bf5f1f909156bd36400e493b491
tarunharness <tarun.israni@harness.io>
2026-02-20 22:49:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import aws_cdk as cdk
 
 from stacks.s3_stack import S3Stack
 from stacks.sqs_stack import SQSStack
-from stacks.ec2_stack import EC2Stack
+# from stacks.ec2_stack import EC2Stack
 
 app = cdk.App()
 S3Stack(app, "S3Stack")
 SQSStack(app, "SQSStack")
-EC2Stack(app, "EC2Stack")
+# EC2Stack(app, "EC2Stack")
 
 app.synth()
